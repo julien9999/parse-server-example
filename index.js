f5efb6087e8e8bcdde208d52baa8c07e40375d79
julien <julien.gatt@hotmail.fr>
2016-08-27 14:33:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,21 @@ var api = new ParseServer({
   appId: process.env.APP_ID || '35rQ21nN4QG8O38lz2H6OSexRC54rUQm',
   masterKey: process.env.MASTER_KEY || 'DHK96A82Li203306k67Ux98chBDfUH39', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://parse-server-ayoa.herokuapp.com',  // Don't forget to change to https if needed
+  
   publicServerURL: 'http://backend.appname.com',
-  // Your apps name. This will appear in the subject and body of the emails that are sent.
   appName: 'appname blah blah',
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+      // The address that your emails come from
+      fromAddress: process.env.MAIL_FROM_ADDRESS,
+      // Your domain from mailgun.com
+      domain: process.env.MAILGUN_DOMAIN,
+      // Your API key from mailgun.com
+      apiKey: process.env.MAILGUN_API_KEY,
+    },
+  
+  
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
