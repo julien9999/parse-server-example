b4d5b90ea31fe2d375042e9a06b4185667d1aaa8
julien <julien.gatt@hotmail.fr>
2016-08-30 19:59:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var api = new ParseServer({
   appId: process.env.APP_ID || '35rQ21nN4QG8O38lz2H6OSexRC54rUQm',
   masterKey: process.env.MASTER_KEY || 'DHK96A82Li203306k67Ux98chBDfUH39', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://parse-server-ayoa.herokuapp.com',  // Don't forget to change to https if needed
-  verifyUserEmails: true,
+  //verifyUserEmails: true,
   publicServerURL: 'https://parse-server-ayoa.herokuapp.com/',
   appName: 'KITapp',
   emailAdapter: { 
