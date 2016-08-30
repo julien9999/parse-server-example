266a9cbeb42db26d54898bd093fddf34d4c094b1
julien <julien.gatt@hotmail.fr>
2016-08-31 00:11:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ var api = new ParseServer({
         templates: {
           passwordResetEmail: {
             subject: 'Reset your password',
-            pathPlainText: resolve(__dirname, 'path/to/templates/password_reset_email.txt'),
-            pathHtml: resolve(__dirname, 'path/to/templates/password_reset_email.html'),
+            pathPlainText: resolve(__dirname, 'public/password_reset_email.txt'),
+            pathHtml: resolve(__dirname, 'public/password_reset_email.html'),
             callback: (user) => { return { firstName: user.get('firstName') }}
             // Now you can use {{firstName}} in your templates
           }
