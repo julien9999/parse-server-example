e388e440a792bc326c90a86cb803b80e18da8dc4
julien <julien.gatt@hotmail.fr>
2017-01-22 18:38:32 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -10,7 +10,7 @@ function randomString(len, charSet) {
 
 Parse.Cloud.afterSave("_User", function(request, response) {
     if (request.object.original) {
-    	request.object.set("kitcode",randomString(4) + randomString(4,"0123456789"));
+    	request.object.set("kitcode",randomString(4) +""+ randomString(4, '0123456789'));
     	response.success();
     }
 });
