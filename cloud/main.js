6823f680cf94f57dbd5d5ebd0e99efb1816f426f
julien <julien.gatt@hotmail.fr>
2017-01-22 18:52:38 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,5 @@
 function randomString(len, charSet) {
-    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     var randomString = '';
     for (var i = 0; i < len; i++) {
         var randomPoz = Math.floor(Math.random() * charSet.length);
@@ -10,7 +10,7 @@ function randomString(len, charSet) {
 
 Parse.Cloud.beforeSave("_User", function(request, response) {
     if (request.object.original) {
-    	request.object.set("kitcode",randomString(4));
+    	request.object.set("kitcode",randomString(5));
     	response.success();
     }
 });
