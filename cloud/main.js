cd67e2dd66ca4e9dfd209024075d20a25f6fda06
julien <julien.gatt@hotmail.fr>
2017-01-22 20:00:28 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -14,10 +14,23 @@ function randomString() {
     return randomString;
 }
 
+function shuffle() {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
+
 Parse.Cloud.beforeSave(Parse.User, function(request, response) {
   var user = request.object;
   if (!user.get("kitcode")) {
-    	request.object.set("kitcode",randomString(4));
+    	request.object.set("kitcode",shuffle(randomString(4)));
     	response.success();
   } else {
     response.success();
